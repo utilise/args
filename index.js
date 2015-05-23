0cb957ac50ba4cca096462c6088c429baca62195
pemrouz <p3pedram@hotmail.com>
2015-05-23 19:01:08 +0100

    destructure for single args

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function args(indices) {
         , a = to.arr(arguments)
                 .filter(function(d,x){ return is.in(i)(x) })
 
-      return fn.apply(this, a)
+      return i.length == 1 ? fn(a[0]) : fn.apply(this, a)
     }
   }
 }
